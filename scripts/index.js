07942206b55bd6dccc4c41cdd6071427c0203a99
rwashingtony@gmail.com <rwash@Reginas-MacBook-Pro.local>
2024-12-26 16:22:29 -0500

    validation form complete

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,13 @@ const initialCards = [
   },
 ];
 
+// Profile form elements
 const profileEditButton = document.querySelector(".profile__edit-btn");
 const cardModalButton = document.querySelector(".profile__add-btn");
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
+// Edit form elements
 const editModal = document.querySelector("#edit-modal");
 const editFormElement = editModal.querySelector(".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
@@ -38,17 +40,21 @@ const editModalDesriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
 
+// Card form elements
 const cardModal = document.querySelector("#add-card-modal");
 const cardFormElement = cardModal.querySelector(".modal__form");
+const cardSaveBtn = cardModal.querySelector(".modal__save-btn");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 
+// Preview image popup elements
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
 const previewModalCloseEl = previewModal.querySelector(".modal__close-btn");
 
+// Card related elements
 const cardTemplate = document.querySelector("#card-template");
 const cardslist = document.querySelector(".cards__list");
 
@@ -106,13 +112,19 @@ function handleCardFormSubmit(evt) {
   const cardElement = getCardElement(inputValues);
   cardslist.prepend(cardElement);
   closeModal(cardModal);
+  disabledButton(cardSaveBtn, settings);
   evt.target.reset();
 }
 
 profileEditButton.addEventListener("click", () => {
   editModalNameInput.value = profileName.textContent;
   editModalDesriptionInput.value = profileDescription.textContent;
-  openModal(editModal);
+  resetValidation(
+    editFormElement,
+    [editModalNameInput, editModalDesriptionInput],
+    settings
+  ),
+    openModal(editModal);
 });
 
 editModalCloseBtn.addEventListener("click", () => {
