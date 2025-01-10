819bd5be608aad9d304b368ecc1c981dee9db22d
rwashingtony@gmail.com <rwash@Reginas-MacBook-Pro.local>
2025-01-10 13:26:01 -0500

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+
+  mode: "development",
+  devtool: "inline-source-map",
+  stats: "errors-only",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|webp|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
+
+babel.config.js;
+
+const presets = [
+  [
+    "@babel/preset-env", // The preset you want to use.
+    {
+      // The browser versions where we want our code to be supported. This could
+      // be adjusted to support more or less different browsers. Refer to
+      // https://babeljs.io/docs/options#targets for details.
+      targets: "defaults, IE 11, not dead",
+
+      // Use polyfills for the browsers specified in the above targets option
+      // Babel uses polyfills from the core-js library
+      useBuiltIns: "entry",
+      corejs: "^3",
+    },
+  ],
+];
+
+module.exports = { presets };
